87076c93ea98f39d0ca1916d34d87d1014de336a
Sudharshna Lakshmi.S <sudharshnalakshmi@gmail.com>
2023-04-05 10:46:59 +0530

    ex04

--- EX04/Program.cs
+++ EX04/Program.cs
@@ -1,26 +1,6 @@
-# Ex04-Constructor
-## Aim:
- To write a C# program to calculate the salary of an employee by passing the name, designation, noofexperience, basic salary and insurance amount through constructor.
-
-## Algorithm:
-### Step1:
-Start the program.
-### Step2:
-Create a class and a constructor
-### Step3:
-Get name, designation, noofexperience, basic salary and insurance amount from the User.
-### Step4:
-call salary method in constructor to calculate salary.
-### Step5:
-call display method to display the output.
-### Step6:
-End the program
-
-## Program:
- ```
- using System;
- public class Employee
- {
+﻿using System;
+public class Employee
+{
     public String designation;
     public String employee_name;
     public int exp;
@@ -51,9 +31,9 @@ End the program
 
     }
 
- }
-  class TestEmployee
-  {
+}
+class TestEmployee
+{
     public static void Main(string[] args)
     {
         Employee e1 = new Employee("SUDHARSHNA", "MANAGER", 8, 300000, 5000);
@@ -64,10 +44,4 @@ End the program
         e2.display();
 
     }
-   }
- ```
- ## Output:
- ![](1.png)
-
- ## Result:
- Thus C# program to calculate the salary of an employee by passing the name, designation, noofexperience, basic salary and insurance amount through constructor is executed successfully.
+}
